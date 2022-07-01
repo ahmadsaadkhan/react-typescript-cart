c24bcb8e61d3dcd96a82e54b9ae2dca683db870a
Ahmad Saad <ahmad.saad.khn@gmail.com>
2022-07-02 01:03:54 +0500

    final commit to deploy

---
+++ src/utilities/utils.ts
@@ -0,0 +1,6 @@
+const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
+    style: 'currency',
+    currency: 'USD',
+})
+
+export const formatCurrency = (value: number) => CURRENCY_FORMATTER.format(value)
